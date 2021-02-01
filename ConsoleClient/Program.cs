17d28c0d691b7cb3033efd54bdfbe2eb14e9ff75
Vladimir Marenich <marenich.vladimir@gmail.com>
2021-02-01 18:39:17 +0200

    Add swagger generated client.

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using ConsoleClient.DeliveryService;
+
+namespace ConsoleClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Test1();
+
+            Console.ReadLine();
+        }
+
+        private static async void Test1()
+        {
+            var client = new Client("https://localhost:44385/", new HttpClient());
+            var all = await client.FillAllAsync();
+        }
+    }
+}
